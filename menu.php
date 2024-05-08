5e36f0a10ba9e473e8099ab71c86c7c26ca4fd20
KhyeeHEN <henkhyee4.0@gmail.com>
2024-05-08 13:30:19 +0800

    dabble in menu php and added settings php for connection to database to retreive data

--- menu.php
+++ menu.php
@@ -7,8 +7,42 @@
 <body class="index1">
     <h1>Products</h1>
     <div class="header1">
-        
+    <?php
+        //connect to database based on credentials in settings.php
+        require_once("settings.php");
+        $dbconn = @mysqli_connect("localhost", "username", "password", "database");
+        if (!$dbconn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        $sql = "SELECT item_id, item_name, imgpath, `desc`, price FROM menu_items";
+        $result = mysqli_query($dbconn, $sql);
+
+        //generate table
+        echo "<table id='menuTable' border='1'>";
+        echo "<thead>";
+        echo "<tr>";
+        echo "<th>Item ID</th>";
+        echo "<th'>Item Name</th>";
+        echo "<th>Image</th>"; 
+        echo "<th'>Description</th>";
+        echo "<th'>Price</th>";
+        echo "</tr>";
+        echo "</thead>";
+        echo "<tbody>";
+
+        while ($row=mysqli_fetch_assoc($result)){
+                echo "<tr>";
+                echo "<td>" . $row['item_id'] . "</td>";
+                echo "<td>" . $row['item_name'] . "</td>";
+                echo "<td><img src='" . $row['imgpath'] . "' alt='" . $row['item_name'] . "' style='width: 100px; height: auto;'></td>"; // Display the image
+                echo "<td>" . $row['desc'] . "</td>";
+                echo "<td>" . $row['price'] . "</td>";
+                echo "</tr>";
+        }
+        echo "</tbody>";
+        echo "</table>";
+        mysqli_close($dbconn);
+    ?>  
     </div>
-    
 </body>
 </html>
---
+++ settings.php
@@ -0,0 +1,6 @@
+<?php
+$host="localhost:3307";
+$user="root";
+$pwd="";
+$sql_db="sweproject_db";
+?>
