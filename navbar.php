7fb0973c41a87f3eb653356b2ff641a52b43fc89
Cylim1507 <121530482+Cylim1507@users.noreply.github.com>
2024-05-10 10:48:25 +0800

    navigation

---
+++ navbar.php
@@ -0,0 +1,12 @@
+<div class="navbar1">             
+    <div class="logo1"><a href="#"><img src="#" alt="logo"></a></div>  
+
+    <nav class="listing1">
+    <ul>
+        <li><a href="#">Category 1</a></li>
+        <li><a href="#">Category 2</a></li>
+        <li><a href="#">Category 3</a></li>
+
+    </ul>
+    </nav>
+</div>
