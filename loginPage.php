a952535201c6a98789af508514dc6e593e5367f8
KhyeeHEN <henkhyee4.0@gmail.com>
2024-05-10 15:08:00 +0800

    starting to add styling to menu

--- loginPage.php
+++ loginPage.php
@@ -3,7 +3,6 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
         <!--CSS-->
         <link rel="stylesheet" href="styling/style.css">
         <title>Log In</title>
