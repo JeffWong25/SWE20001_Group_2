48d8d981b2b323cb4c126ef788b2fc9d19d5d126
KhyeeHEN <henkhyee4.0@gmail.com>
2024-05-10 16:39:20 +0800

    Update menu.js

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,18 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Code inside this function will run after the DOM content is loaded
-    function filterTable(categoryId) {
-        var table, rows, i, categoryValue;
-        table = document.getElementById("menu-table");
-        if (table) { // Check if table exists
-            rows = table.getElementsByTagName("tr");
-            for (i = 0; i < rows.length; i++) {
-                categoryValue = rows[i].getAttribute("data-category");
-                if (categoryValue === String(categoryId)) {
-                    rows[i].style.display = "";
-                } else {
-                    rows[i].style.display = "none";
-                }
-            }
+function filterTable(categoryId) {
+    var table, rows, i, categoryValue;
+    table = document.getElementById("menu-table");
+    rows = table.getElementsByTagName("tr");
+    for (i = 1; i < rows.length; i++) {
+        categoryValue = rows[i].getAttribute("data-category");
+        if (categoryValue === String(categoryId)) {
+            rows[i].style.display = "";
+        } else {
+            rows[i].style.display = "none";
         }
     }
-});
+}
