45db14779922c97e7fa1f0b21bb18ce3ecd5911a
KhyeeHEN <henkhyee4.0@gmail.com>
2024-05-12 18:03:28 +0800

    Update menu.js

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,7 +2,7 @@ function filterTable(categoryId) {
     var table, rows, i, categoryValue;
     table = document.getElementById("menu-table");
     rows = table.getElementsByTagName("tr");
-    for (i = 1; i < rows.length; i++) {
+    for (i = 0; i < rows.length; i++) {
         categoryValue = rows[i].getAttribute("data-category");
         if (categoryValue === String(categoryId)) {
             rows[i].style.display = "";
