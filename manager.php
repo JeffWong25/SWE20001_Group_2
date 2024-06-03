626f07f06de36e7d22b1ece05e3c64b4ec225a1b
KhyeeHEN <henkhyee4.0@gmail.com>
2024-06-03 11:49:42 +0800

    added manager php

---
+++ manager.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<div class ="menu-whole-page">
+<head class="bg">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BurgerBytes Menu</title>
+    <link rel="stylesheet" href="styling/style.css">
+    <link rel="stylesheet2" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="scripts/menu.js"></script>
+</head>
+<body class="menu-body">
+    <div class="menu-header">
+        <div class="menu-header-left">
+            <a><img src="images\vecteezy_burger-vector-logo-template-in-line-style-burger-simple-icon_7714606.png" id="logo" alt="BurgerBytes logo" width="80"></a>
+            <h1>BurgerBytes Products</h1>
+        </div>
+       <a href="logout.php" class="logout-button">Logout</a>
+    </div>
