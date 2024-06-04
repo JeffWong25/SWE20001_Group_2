d13c39f55810c8c2c25b352c5c1798638a838b13
KhyeeHEN <henkhyee4.0@gmail.com>
2024-06-04 22:08:51 +0800

    Update edit_item.php

--- edit_item.php
+++ edit_item.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BurgerBytes Menu</title>
     <link rel="stylesheet" href="styling/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">   
+    <link rel="stylesheet2" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">   
 </head>
 <body class="menu-body">
     <div class="menu-header">
