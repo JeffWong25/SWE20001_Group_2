0c5d2ad6fcbb3c5492b0ac55c15dcf34a03bc335
Cylim1507 <121530482+Cylim1507@users.noreply.github.com>
2024-06-06 00:23:29 +0800

    Create kitchen.php

---
+++ kitchen.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<div class ="menu-whole-page">
+<head class="bg">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BurgerBytes Manager Menu</title>
+    <link rel="stylesheet" href="styling/style.css">
+    <link rel="stylesheet2" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="scripts/menu.js"></script>
+</head>
+<body class="menu-body">
+    <div class="menu-header">
+        <div class="menu-header-left">
+            <a><img src="images\vecteezy_burger-vector-logo-template-in-line-style-burger-simple-icon_7714606.png" id="logo" alt="BurgerBytes logo" width="80"></a>
+            <h1>BurgerBytes Menu</h1>
+        </div>
+         <!-- To whoever is making the manager login stuff, change this href to the actual staff login interface -->
+        <a href="logout.php" class="logout-button">Logout</a>
+    </div>
+
+    
+    
+    <footer class="menu-footer">
+        <div class="menu-footer-content">
+            <div class="menu-footer-left">
+                <p>&copy; 2024 BurgerBytes. All rights reserved.</p>
+            </div>
+            <div class="menu-footer-right">
+                <p>Contact Us: burgerbytes@gmail.com</p>
+            </div>
+        </div>
+    </footer>
+</body>
+</div>
+</html>
